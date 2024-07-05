9237d35f8f6139758783c2e38bf2e12033ccf9f4
juan hernandez <juansys93@gmail.com>
2024-07-04 22:46:13 -0600

    proyecto inicial

---
+++ src/main/java/com/serti/pokedex/SertiDexApplication.java
@@ -0,0 +1,13 @@
+package com.serti.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertiDexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertiDexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serti/pokedex/SertiDexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serti.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SertiDexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
